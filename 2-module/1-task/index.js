b4c9b37325b77e04adcd54adacd62ec1136e9fb5
Hilarey <Piatnytskiy.y.m@gmail.com>
2021-10-13 12:17:24 +0300

    HW-02_all_done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (
+      typeof salaries[key] === "number" &&
+      !isNaN(salaries[key]) &&
+      salaries[key] !== Infinity &&
+      salaries[key] !== -Infinity
+    ) {
+      sum += salaries[key];
+    } else if (salaries[key] === 0) {
+      sum = 0;
+    }
+  }
+  return sum;
 }
